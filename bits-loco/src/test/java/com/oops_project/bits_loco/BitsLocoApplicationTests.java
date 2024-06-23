c0c8275faaac1b3280488b6c48510c1817e67ac8
Manas Ashwin <37976250+manaschubby@users.noreply.github.com>
2024-06-23 20:15:24 +0530

    feat: Init backend

---
+++ bits-loco/src/test/java/com/oops_project/bits_loco/BitsLocoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oops_project.bits_loco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitsLocoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
